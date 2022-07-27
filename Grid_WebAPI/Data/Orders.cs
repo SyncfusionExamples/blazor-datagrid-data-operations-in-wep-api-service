680d75d7f4c913a8c39802c45521be4f56d61912
Vignesh Natarajan <vignesh.natarajan@syncfusion.com>
2022-07-27 14:54:31 +0530

    sample

---
+++ Grid_WebAPI/Data/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Grid_WebAPI.Data
+{
+    public class Order
+    {
+        [Key]
+        public int? OrderID { get; set; }
+        [Required]
+        public string CustomerID { get; set; }
+        [Required]
+        public int EmployeeID { get; set; }
+
+    }
+}
